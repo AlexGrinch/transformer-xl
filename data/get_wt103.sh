4a5ffe5ef23812969f21f71306fa59d58f5961cb
Aleksey Grinchuk (Oleksii Hrinchuk) <grinchuk.alexey@gmail.com>
2019-01-12 14:00:55 -0800

    Update get_wt103.sh

--- data/get_wt103.sh
+++ data/get_wt103.sh
@@ -2,15 +2,14 @@ echo "=== Acquiring datasets ==="
 echo "---"
 
 echo "- Downloading WikiText-103 (WT2)"
-if [[ ! -d 'wikitext-103' ]]; then
-    wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
-    unzip -q wikitext-103-v1.zip
-    cd wikitext-103
-    mv wiki.train.tokens train.txt
-    mv wiki.valid.tokens valid.txt
-    mv wiki.test.tokens test.txt
-    cd ..
-fi
+
+wget --continue https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
+unzip -q wikitext-103-v1.zip
+cd wikitext-103
+mv wiki.train.tokens train.txt
+mv wiki.valid.tokens valid.txt
+mv wiki.test.tokens test.txt
+cd ..
 
 echo "---"
 echo "Happy language modeling :)"

42150ae4a79e4300f1fff0eb5d37ed00319c9332
AlexGrinch <grinchuk.alexey@gmail.com>
2019-01-19 01:12:01 +0000

    added obw script

---
+++ data/prepare_lm1b.sh
@@ -0,0 +1,19 @@
+echo "=== Acquiring datasets ==="
+echo "---"
+
+mkdir -p one-billion-words
+cd one-billion-words
+
+cp /dataset/1-billion-word-language-modeling-benchmark-r13output.tar.gz .
+tar xzvf 1-billion-word-language-modeling-benchmark-r13output.tar.gz
+
+path="1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled/"
+cat ${path}/news.en.heldout-00000-of-00050 > valid.txt
+cat ${path}/news.en.heldout-00000-of-00050 > test.txt
+
+wget https://github.com/rafaljozefowicz/lm/raw/master/1b_word_vocab.txt
+
+cd ..
+
+echo "---"
+echo "Happy language modeling :)"

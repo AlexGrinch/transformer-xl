a11bfd13664779f03e116915a071740e0819adb8
Aleksey Grinchuk (Oleksii Hrinchuk) <grinchuk.alexey@gmail.com>
2019-01-18 17:44:07 -0800

    Update prepare_lm1b.sh

--- data/prepare_lm1b.sh
+++ data/prepare_lm1b.sh
@@ -5,7 +5,7 @@ mkdir -p one-billion-words
 cd one-billion-words
 
 cp /dataset/1-billion-word-language-modeling-benchmark-r13output.tar.gz .
-tar xzvf 1-billion-word-language-modeling-benchmark-r13output.tar.gz
+tar xzvf 1-billion-word-language-modeling-benchmark-r13output.tar.gz --owner root --group root --no-same-owner 2>&1 > tar2.log
 
 path="1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled/"
 cat ${path}/news.en.heldout-00000-of-00050 > valid.txt
